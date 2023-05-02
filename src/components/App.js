fd2ff2a4512942ea17f1faf2c13befa72d1b9575
Gururaj-RP <115164350+Gururaj-RP@users.noreply.github.com>
2023-05-02 15:11:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Male from "./Male";
+import Female from "./Female";
 
 const App = () => {
+  const [gender,setGender]=useState('male');
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>Select your gender:</h2>
+        <input onClick={()=>setGender('male')} type='radio' name='gender' id='male' defaultChecked/><label>Male</label>
+        <input onClick={()=>setGender('female')} type='radio' name='gender' id='female'/><label>Female</label>
+        {
+          (gender=='male')
+          ?<Male/>
+          :<Female/>
+        }
     </div>
   )
 }
